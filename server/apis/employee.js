88974008acadd994d002c6024ece20013759341c
JAIME GALLO <jagallob@eafit.edu.co>
2024-07-28 15:32:01 -0500

    Crear api employee

--- server/apis/employee.js
+++ server/apis/employee.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const { readJsonFile } = require("../utils/json-reader");
+const employeeInfoJSON = require("../data/employee-info.json");
 
-const getEmployee = async;
-
-//TODO
+const getEmployee = async (request, response) => {
+  try {
+    const employeeInfoJSON = await readJsonFile(
+      process.env.JSON_DIR_EMPLOYEE_INFO
+    );
+    response.status(200).send(employeeInfoJSON);
+  } catch (error) {
+    response.status(400);
+  }
+};
 
 module.exports = {
   getEmployee,
