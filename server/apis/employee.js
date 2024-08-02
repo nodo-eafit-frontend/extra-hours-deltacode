8ff53c29ee56ae3f75973cde6b57e75189276a8e
Marvin Cely <mardancely@gmail.com>
2024-08-01 20:36:00 -0500

    feat: hanlde getEmployee error

--- server/apis/employee.js
+++ server/apis/employee.js
@@ -1,24 +1,30 @@
-require("dotenv").config();
-const { readJsonFile } = require("../utils/json-reader");
+require('dotenv').config();
+const { readJsonFile } = require('../utils/json-reader');
 //const employeeInfoJSON = require("../data/employee-info.json");
 
 const getEmployee = async (request, response) => {
-  const id = Number(request.params.id);
-  try {
-    let employeeInfoJSON = [];
+	const id = Number(request.params.id);
+	try {
+		let employeeInfoJSON = [];
 
-    employeeInfoJSON = await readJsonFile(process.env.JSON_DIR_EMPLOYEE_INFO);
+		employeeInfoJSON = await readJsonFile(process.env.JSON_DIR_EMPLOYEE_INFO);
 
-    const employyeFound = employeeInfoJSON.find((employee) => {
-      return employee.id === id;
-    });
+		const employyeFound = employeeInfoJSON.find((employee) => {
+			return employee.id === id;
+		});
 
-    response.status(200).send(employyeFound);
-  } catch (error) {
-    response.status(400);
-  }
+		if (!employyeFound) {
+			throw new Error('Emplyee not Found');
+		}
+
+		response.status(200).send(employyeFound);
+	} catch (error) {
+		console.log(error);
+
+		response.sendStatus(400);
+	}
 };
 
 module.exports = {
-  getEmployee,
+	getEmployee,
 };
