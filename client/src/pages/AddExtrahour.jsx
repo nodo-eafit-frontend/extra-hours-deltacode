58542af82d9b98db1fd60894233d3071d86e4d26
JAIME GALLO <jagallob@eafit.edu.co>
2024-08-22 23:28:55 -0500

    Ajustes logo

--- client/src/pages/AddExtrahour.jsx
+++ client/src/pages/AddExtrahour.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./AddExtrahour.scss";
 import background from "../assets/images/background.png";
 import { FormExtraHour } from "../components/FormExtraHour/FormExtraHour";
-
+import logo from "../../../server/public/images/logo.png";
 
 const AddExtrahour = () => {
   return (
@@ -10,15 +10,15 @@ const AddExtrahour = () => {
       <div
         style={{
           backgroundImage: `url(${background})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          height: '100vh',
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          height: "100vh",
         }}
       >
         <header className="page__header">
           <a href="http://localhost:5173/">
-            <img src="http://localhost:4000/images/logo.png" alt="Logo" />
+            <img src={logo} />
             {/* <h1 className="heading">Horas Extra Amadeus</h1> */}
           </a>
         </header>
--- client/src/pages/ApprovePage.jsx
+++ client/src/pages/ApprovePage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./ApprovePage.scss";
 import { Approve } from "../components/Approve/Approve";
+import logo from "../../../server/public/images/logo.png";
 const ApproveExtrahour = () => {
   return (
     <>
       <header className="page__header">
         <a href="http://localhost:5173/">
-          <img src="http://localhost:4000/images/logo.png" alt="Logo" />
+          <img src={logo} />
           {/* <h1 class="heading">Horas Extra Amadeus</h1> */}
         </a>
       </header>
 
-      <h2>Aprobar horas extra</h2>
-      {/* <ExtraHoursInfo /> */}
+      <h2>Aprobar - Eliminar horas extra</h2>
+      <Approve />
     </>
   );
 };
